3e7e8b8e8704d8f6ef474579c0a492ff34b79583
santicabrera19 <cabrerasanty321@gmail.com>
2025-07-14 00:22:16 -0300

    fix: excluir utn-logo.png del middleware para permitir acceso a archivos estáticos

--- middleware.ts
+++ middleware.ts
@@ -83,7 +83,8 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - utn-logo.png (logo UTN)
      */
-    '/((?!_next/static|_next/image|favicon.ico).*)',
+    '/((?!_next/static|_next/image|favicon.ico|utn-logo.png).*)',
   ],
 }
